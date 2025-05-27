77e670d671559fd8115f5a2bed7d2a4304988294
tiendungdayyy <91593609+tiendungdayyy@users.noreply.github.com>
2025-05-28 00:37:41 +0700

    feat: add DbContext,ConnectionString

--- SWP_SchoolMedicalManagementSystem_API/Program.cs
+++ SWP_SchoolMedicalManagementSystem_API/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using SWP_SchoolMedicalManagementSystem_BussinessOject.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+#region DBContext
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlServer(configuration.GetConnectionString("local")));
+#endregion
 
 var app = builder.Build();
 
--- SWP_SchoolMedicalManagementSystem_BussinessProject/Entity/HealthCheckupResults.cs
+++ SWP_SchoolMedicalManagementSystem_BussinessProject/Entity/HealthCheckupResults.cs
@@ -11,15 +11,15 @@ namespace SWP_SchoolMedicalManagementSystem_BussinessOject.Entity
         public Guid ScheduleId { get; set; } 
         public StudentHealthCheckupSchedule? HealthCheckupSchedule { get; set; }
         public DateTime CheckupDate { get; set; }
-        public decimal? Height { get; set; } 
-        public decimal? Weight { get; set; } 
+        public float? Height { get; set; } 
+        public float? Weight { get; set; } 
         public string? VisionLeftResult { get; set; }
         public string? VisionRightResult { get; set; }
         public string? HearingLeftResult { get; set; }
         public string? HearingRightResult { get; set; }
-        public int? BloodPressureSys { get; set; } 
-        public int? BloodPressureDia { get; set; } 
-        public int? HeartRate { get; set; } 
+        public float? BloodPressureSys { get; set; } 
+        public float? BloodPressureDia { get; set; } 
+        public float? HeartRate { get; set; } 
         public string? DentalCheckupResult { get; set; }
         public string? OtherResults { get; set; }
         public string? AbnormalSigns { get; set; }
