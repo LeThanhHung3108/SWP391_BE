72048ccdc5d5551f9369cf8b5f3160a3e8af0bcf
tiendungdayyy <91593609+tiendungdayyy@users.noreply.github.com>
2025-05-31 19:21:00 +0700

    create login and register funtion

--- SWP_SchoolMedicalManagementSystem_API/Program.cs
+++ SWP_SchoolMedicalManagementSystem_API/Program.cs
@@ -1,11 +1,34 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using SWP_SchoolMedicalManagementSystem_BussinessOject.Context;
+using SWP_SchoolMedicalManagementSystem_BussinessOject.MapperProfile;
+using SWP_SchoolMedicalManagementSystem_Service.Extension;
+using SWP_SchoolMedicalManagementSystem_Service.IRepository;
+using SWP_SchoolMedicalManagementSystem_Service.IService;
+using SWP_SchoolMedicalManagementSystem_Service.Repository;
+using SWP_SchoolMedicalManagementSystem_Service.Service;
+using System.Reflection; // Added for Assembly reference
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddAutoMapper(typeof(MapperEntities));
+
+#region AddScope
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ITokenGeneratior, TokenGenerator>();
+builder.Services.AddScoped<IUserService, UserService>();
+#endregion
+
 #region DBContext
 IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
@@ -15,12 +38,76 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseSqlServer(configuration.GetConnectionString("local")));
 #endregion
 
+#region JWT
+var jwtSettings = configuration.GetSection("JWT");
+var issuer = jwtSettings["Issuer"];
+var audience = jwtSettings["Audience"];
+var secretKey = jwtSettings["SecretKey"];
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = issuer,
+            ValidateAudience = true,
+            ValidAudience = audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+#endregion
+
+#region Swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo() { Title = "SchoolMedical", Version = "v1" });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        new string[] { }
+    }
+    });
+    options.MapType<TimeOnly>(() => new OpenApiSchema
+    {
+        Type = "string",
+        Format = "time",
+        Example = OpenApiAnyFactory.CreateFromJson("\"13:45:42\"")
+    });
+});
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthorization();
-
+app.UseAuthentication();
 app.MapControllers();
 
 app.Run();
---
+++ SWP_SchoolMedicalManagementSystem_Service/Service/UserService.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using SWP_SchoolMedicalManagementSystem_BussinessOject.Dto.AuthDto;
+using SWP_SchoolMedicalManagementSystem_BussinessOject.Dto.UserDto;
+using SWP_SchoolMedicalManagementSystem_BussinessOject.Entity;
+using SWP_SchoolMedicalManagementSystem_Service.Extension;
+using SWP_SchoolMedicalManagementSystem_Service.IRepository;
+using SWP_SchoolMedicalManagementSystem_Service.IService;
+
+namespace SWP_SchoolMedicalManagementSystem_Service.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        private readonly ITokenGeneratior _tokenGenerator;
+
+        public UserService(IHttpContextAccessor httpContextAccessor, IUserRepository userRepository, IMapper mapper, ITokenGeneratior tokenGeneratior)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _userRepository = userRepository;
+            _mapper = mapper;
+            _tokenGenerator = tokenGeneratior;
+        }
+
+        public Task DeleteUserAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserResponseDto>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserResponseDto?> GetUserByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserResponseDto?> GetUserByIdAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<AuthResponseDto> Login(AuthRequestDto request)
+        {
+            var user = _userRepository.GetUserByUsernameAsync(request.Username).Result;
+            if (user == null || user.Password != request.Password)
+            {
+                throw new UnauthorizedAccessException("Invalid username or password.");
+            }
+            return await _tokenGenerator.GenerateToken(user, user.UserRole.ToString());
+        }
+
+        public async Task Register(UserRegisterRequestDto request)
+        {
+            var user = _mapper.Map<User>(request);
+            user.UserRole = SchoolMedicalManagementSystem.Enum.UserRole.Parent;
+            await _userRepository.AddUserAsync(user);
+        }
+
+        public Task UpdateUserAsync(Guid id, UserRegisterRequestDto request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
