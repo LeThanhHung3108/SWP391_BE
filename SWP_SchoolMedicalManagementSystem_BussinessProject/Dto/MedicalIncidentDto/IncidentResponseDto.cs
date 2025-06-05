2ad5bebcaa16d353758f6e79425d2d00f414281d
tiendungdayyy <91593609+tiendungdayyy@users.noreply.github.com>
2025-06-05 14:43:00 +0700

    MedicalSupply CRUD

---
+++ SWP_SchoolMedicalManagementSystem_BussinessProject/Dto/MedicalIncidentDto/IncidentResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using SchoolMedicalManagementSystem.Enum;
+using SWP_SchoolMedicalManagementSystem_BussinessOject.Entity;
+
+namespace SWP_SchoolMedicalManagementSystem_BussinessOject.Dto.MedicalIncidentDto
+{
+    public class IncidentResponseDto
+    {
+        public Guid Id { get; set; }
+        public Student? Student { get; set; }
+        public IncidentType IncidentType { get; set; }
+        public DateTime IncidentDate { get; set; }
+        public string? Description { get; set; }
+        public string? ActionsTaken { get; set; }
+        public string? Outcome { get; set; }
+        public IncidentStatus Status { get; set; }
+        public bool ParentNotified { get; set; }
+        public DateTime ParentNotificationDate { get; set; }
+    }
+}
---
+++ SWP_SchoolMedicalManagementSystem_Service/IRepository/IMedicalSupplierRepository.cs
@@ -0,0 +1,13 @@
+﻿using SWP_SchoolMedicalManagementSystem_BussinessOject.Entity;
+
+namespace SWP_SchoolMedicalManagementSystem_Service.IRepository
+{
+    public interface IMedicalSupplierRepository
+    {
+        Task CreateSupplierAsync(MedicalSupplier supplier);
+        Task UpdateSupplierAsync(MedicalSupplier supplier);
+        Task DeleteSupplierAsync(Guid id);
+        Task<MedicalSupplier?> GetSupplierByIdAsync(Guid id);
+        Task<IEnumerable<MedicalSupplier>> GetAllSuppliersAsync();
+    }
+}
